31d35018b3319c51d9987d364b39b5ff4534ffbd
Adrian <a.milewski@onet.eu>
2021-04-22 12:11:30 +0200

    Changed to english names

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -10,19 +10,22 @@ const useStyles = makeStyles(theme => ({
 export default function FilmCard({ film, handlePreview }) {
     const classes = useStyles(film)
     const [filmData, setFilmData] = useState({})
+    const language = 'en-en'
+    console.log('null?', film == null)
 
     useEffect(async () => {
         const title = film.title
-        const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=a11ffe1b32709f5551e64f4683d948a3&query=${title}&language=pl-pl`)
+        const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=a11ffe1b32709f5551e64f4683d948a3&query=${title}&language=${language}`)
         const film_data = await res.json()
         const element = film_data.results[0]
 
-        setFilmData(element)
+        if (element)
+            setFilmData(element)
     }, [])
 
     return(
         <div>
-            {filmData['original_title']}
+            {filmData['title']}
         </div>
     )
 }
